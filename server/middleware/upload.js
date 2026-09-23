e51dca58388550da7ff456ce5f93d99126a69554
vuongthecuong99 <vuongthecuong99@g.ucla.edu>
2026-09-22 18:37:08 -0700

    Fix uploads directory creation on server

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,5 +1,11 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
