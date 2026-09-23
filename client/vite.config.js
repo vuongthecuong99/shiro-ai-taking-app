c08e964eb2bb2ba147ed8af0c87a6aa60493655d
vuongthecuong99 <vuongthecuong99@g.ucla.edu>
2026-09-22 20:16:31 -0700

    Add offline support via service worker

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, // we already have our own manifest.json
+      includeAssets: ['icon-192.png', 'icon-512.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      }
+    })
+  ],
 })
